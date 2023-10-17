4aaa89d0f63f62334d8667ab8e8f232b1e690c06
Ryo <keyring1231@gmail.com>
2023-10-18 07:49:44 +0900

    feat: add jwt

---
+++ src/lib/server/jwt.ts
@@ -0,0 +1,33 @@
+import { JWT_SECRET } from "$env/static/private";
+import { error } from "@sveltejs/kit";
+import * as jose from "jose";
+
+type JWTPayload = {
+  firstName: string;
+  lastNmae: string;
+  email: string;
+  id: number;
+};
+
+export const createAuthJWT = async (data: JWTPayload) => {
+  try {
+    const jwt = await new jose.SignJWT(data)
+      .setProtectedHeader({ alg: "HS256"})
+      .sign(new TextEncoder().encode(JWT_SECRET));
+    return jwt;
+  } catch(err) {
+    throw error(401, "failed create JWT...")
+  }
+};
+
+export const verifyAuthJWT = async (token: string) => {
+  try {
+    const { payload } = await jose.jwtVerify(
+      token,
+      new TextEncoder().encode(JWT_SECRET)
+    );
+    return payload as JWTPayload;
+  } catch {
+    throw error(401, "invalid or missing JWT, you are not logged in")
+  }
+}
