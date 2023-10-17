049d15be8093761c7476820c4052e965d1a998c4
Ryo <keyring1231@gmail.com>
2023-10-17 21:55:27 +0900

    feat: conect db

---
+++ src/lib/server/db.ts
@@ -0,0 +1,7 @@
+import { drizzle } from 'drizzle-orm/postgres-js'
+import postgres from 'postgres'
+import { DATABASE_URL } from "$env/static/private";
+
+const connectionString = DATABASE_URL
+const client = postgres(connectionString)
+export const db = drizzle(client);
