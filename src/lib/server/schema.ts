4d231f565f8d667edd58f4eaa91b54c18215017e
Ryo <keyring1231@gmail.com>
2023-10-18 08:50:33 +0900

    feat: define schema

---
+++ src/lib/server/schema.ts
@@ -0,0 +1,36 @@
+import { relations } from "drizzle-orm";
+import {
+  boolean,
+  integer,
+  pgTable,
+  serial,
+  text,
+  varchar
+} from "drizzle-orm/pg-core";
+
+export const usersTable = pgTable("users", {
+  id: serial("id").primaryKey(),
+  first_name: varchar("first_name", { length: 255 }).notNull(),
+  last_name: varchar("last_name", { length: 255 }).notNull(),
+  email: varchar("password", { length: 255 }).notNull()
+});
+
+export const todosTable = pgTable("todos", {
+  id: serial("id").primaryKey(),
+  title: varchar("title", { length: 255 }).notNull(),
+  description: text("description"),
+  completed: boolean("completed").notNull(),
+  user_id: integer("user_id").notNull()
+});
+
+export const usersTableRelations = relations(usersTable, ({ many }) => ({
+	todos: many(todosTable),
+}));
+
+export const todosTableRelations = relations(todosTable, ({ one }) => ({
+  user: one(usersTable, {
+    fields: [todosTable.user_id],
+    references: [usersTable.id]
+  })
+}));
+
